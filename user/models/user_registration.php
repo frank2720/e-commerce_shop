e203db45eb972c5cc8ab9bcca081cfa109b5b884
frank2720 <otienof534@gmail.com>
2023-09-19 19:07:55 +0300

    authentication system update

---
+++ user/models/user_registration.php
@@ -0,0 +1,41 @@
+<?php
+include_once '../database/connection.php';
+
+function new_user_input($data) {
+	
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+	$username = new_user_input($_POST["username"]);
+    $phone=new_user_input($_POST['phone']);
+    $email=new_user_input($_POST['email']);
+	$password = new_user_input($_POST["password"]);
+    $cpassword = new_user_input($_POST['confirm_password']);
+
+    $stmt2 = $conn->prepare("SELECT * FROM users WHERE username='$username'");
+	$stmt2->execute();
+	$count = $stmt2->rowCount();
+    
+    if($count==0) {
+        if($password==$cpassword){
+            $hash = password_hash($password,PASSWORD_DEFAULT);
+            #using password hashing
+            $stmt1 = $conn->prepare("INSERT INTO users (username,password,email,phone) VALUES ('$username','$hash','$email','$phone')");
+            if($stmt1->execute()){
+                echo "<script>alert('account created successfully')</script>";
+            }
+        }else{
+            echo "<script>alert('password does not match')</script>"; 
+        }
+    }
+    elseif ($count>0) {
+        echo "<script>alert('username available')</script>";
+    } 
+	
+}
+?>
--- user/register.php
+++ user/register.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'models/user.php';
+include_once 'models/user_registration.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
