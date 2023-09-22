0439e69c43d99062a94e67613a2b5e26538e4677
frank2720 <otienof534@gmail.com>
2023-09-22 13:13:01 +0300

    product details update

--- product_details.php
+++ product_details.php
@@ -41,7 +41,7 @@ include_once 'functions/common.php';
             <!-- Left links -->
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item active">
-                    <a class="nav-link" href="https://mdbootstrap.com/">Home</a>
+                    <a class="nav-link" href=""></a>
                 </li>
             </ul>
             <!-- Left links -->
