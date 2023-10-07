4612b96c5695a556e997a9707c6d49cc120ea578
frank2720 <otienof534@gmail.com>
2023-10-07 16:03:54 +0300

    Registration system

--- userauth/register.php
+++ userauth/register.php
@@ -1,64 +1,38 @@
+
 <?php
-include_once '../database/connection.php';
 
-// Now we check if the data was submitted, isset() function will check if the data exists.
-if (!isset($_POST['username'], $_POST['phone'], $_POST['email'], $_POST['password'])) {
-	// Could not get the data that should have been sent.
-	exit('Please complete the registration form!');
-}
-// Make sure the submitted registration values are not empty.
-if (empty($_POST['username']) || empty($_POST['phone']) || empty($_POST['email']) || empty($_POST['password'])) {
-	// One or more values are empty.
-	exit('Please complete the registration form');
-}
-
-function new_user_input($data) {
-	
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	
-	$username = new_user_input($_POST["username"]);
-    $phone=new_user_input($_POST['phone']);
-    $email=new_user_input($_POST['email']);
-	$password = new_user_input($_POST["password"]);
-    $cpassword = new_user_input($_POST["confirm_password"]);
-    $code = uniqid();
-
-    $stmt2 = $conn->prepare("SELECT * FROM accounts WHERE username='$username'");
-	$stmt2->execute();
-	$count = $stmt2->rowCount();
-    
-    if($count==0) {
-        if($password==$cpassword){
-            $hash = password_hash($password,PASSWORD_DEFAULT);
-            #using password hashing
-            $stmt1 = $conn->prepare("INSERT INTO accounts (username,phone,email,password,activation_code) VALUES ('$username','$phone','$email','$hash','$code')");
-            if($stmt1->execute()){
-                $from    = 'noreply@yourdomain.com';
-                $subject = 'Account Activation Required';
-                $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
-                // Update the activation variable below
-                $activate_link = 'http://yourdomain.com/phplogin/activate.php?email=' . $_POST['email'] . '&code=' . $code;
-                $message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
-                mail($_POST['email'], $subject, $message, $headers);
-                echo 'Please check your email to activate your account!';
-            }
-        }else{
-            echo "<script>alert('password does not match')</script>"; 
-            include_once 'register.html';
-
-        }
-    }
-    elseif ($count>0) {
-        echo "<script>alert('username available')</script>";
-        include_once 'register.html';
-    } 
-	
-}
-
+require __DIR__ . '/../src/bootstrap.php';
 ?>
+
+<?php view('header', ['title' => 'Register']) ?>
+
+    <form action="register.php" method="post">
+        <h1>Sign Up</h1>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <div>
+            <label for="password2">Password Again:</label>
+            <input type="password" name="password2" id="password2">
+        </div>
+        <div>
+            <label for="agree">
+                <input type="checkbox" name="agree" id="agree" value="yes"/> I agree
+                with the
+                <a href="#" title="term of services">term of services</a>
+            </label>
+        </div>
+        <button type="submit">Register</button>
+        <footer>Already a member? <a href="login.php">Login here</a></footer>
+    </form>
+
+<?php view('footer') ?>
