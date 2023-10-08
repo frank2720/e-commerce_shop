0f01db97c400504af033162eb7318fa226300a71
frank2720 <otienof534@gmail.com>
2023-10-08 03:04:46 +0300

    Registration system

--- userauth/src/inc/header.php
+++ userauth/src/inc/header.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
-    <title><?= $title ?? 'Pudfra'?></title>
+    <title><?= $title ?? 'Home' ?></title>
 </head>
 <body>
 <main>
+<?php flash() ?>
