a6999bee13e59315e1764242b7ff9adf8f6d9d42
frank2720 <otienof534@gmail.com>
2023-10-08 11:06:36 +0300

    Registration system

--- userauth/src/auth.php
+++ userauth/src/auth.php
@@ -37,6 +37,27 @@ function find_user_by_username(string $username)
     return $statement->fetch(PDO::FETCH_ASSOC);
 }
 
+function login(string $username, string $password): bool
+{
+    $user = find_user_by_username($username);
+
+    // if user found, check the password
+    if ($user && password_verify($password, $user['password'])) {
+
+        // prevent session fixation attack
+        session_regenerate_id();
+
+        // set username in the session
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['user_id']  = $user['id'];
+
+
+        return true;
+    }
+
+    return false;
+}
+
 function is_user_logged_in(): bool
 {
     return isset($_SESSION['username']);
