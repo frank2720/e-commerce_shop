879b0db5b4197d64c9a9ce34193f5ea0e3d80c3d
frank2720 <otienof534@gmail.com>
2023-10-14 01:55:01 +0300

    products page[A updating

--- src/inc/page_footer.php
+++ src/inc/page_footer.php
@@ -2,7 +2,7 @@
 <footer class="text-center text-white mt-4" style="background-color: #607D8B">
 
   <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2); text-color: #E0E0E0">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
   <p>&copy; <?=date('Y')?>, Pudfra-Shop</p>
   </div>
   <!-- Copyright -->
