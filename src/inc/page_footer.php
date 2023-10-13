005795b2ac7d48a9e29c25cb8693d1d51a56387a
frank2720 <otienof534@gmail.com>
2023-10-13 05:04:11 +0300

    Page styling and price updating

--- src/inc/page_footer.php
+++ src/inc/page_footer.php
@@ -6,9 +6,6 @@
   </div>
   <!-- Copyright -->
 </footer>
-    <!-- End your project here-->
-
-    <!-- MDB -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
   </body>
 </html>
