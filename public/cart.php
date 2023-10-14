93da12a5dac8880ac6dd096ef7df3888bc418c29
frank2720 <otienof534@gmail.com>
2023-10-14 18:08:56 +0300

    cart page updating

--- public/cart.php
+++ public/cart.php
@@ -26,70 +26,204 @@ if ($products_in_cart) {
 
 <?php view('page_header', ['title' => 'Cart details']) ?>
 
-<header>
-    <div class="content-wrapper">
-        <h1>Pudfra-Shop</h1>
-        <nav>
-            <a href="home.php">Home</a>
-            <a href="products.php">Products</a>
-        </nav>
-        <div class="link-icons">
-            <a href="cart.php">
-                <i class="fas fa-shopping-cart"></i><span><?=$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;?></span>
-            </a>
-        </div>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+  <!-- Container wrapper -->
+  <div class="container">    
+    <!-- Toggle button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent1"
+      aria-controls="navbarSupportedContent1"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent1">      
+      <!-- Navbar brand -->
+      
+      <!-- Left links -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link " href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="products.php">Products</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-mdb-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Categories
+          </a>
+          <!-- Dropdown menu -->
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <?=getcategories()?>
+          </ul>
+        </li>
+      </ul>
+      <!-- Left links -->      
     </div>
-</header>
-<main>
-<div class="cart content-wrapper">
-    <form action="cart.php" method="post">
-        <table>
-            <thead>
-                <tr>
-                    <td colspan="2">Product</td>
-                    <td>Price</td>
-                    <td>Quantity</td>
-                    <td>Total</td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (empty($products)): ?>
-                <tr>
-                    <td colspan="5" style="text-align:center;">You have no products added in your Shopping Cart</td>
-                </tr>
-                <?php else: ?>
-                <?php foreach ($products as $product): ?>
-                <tr>
-                    <td class="img">
-                        <a href="product_details.php?product_id=<?=$product['product_id']?>">
-                            <img src="admin_page/actions/<?=$product['product_image']?>" width="50" height="50" alt="<?=$product['product_name']?>">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="product_details.php?product_id=<?=$product['product_id']?>"><?=$product['product_name']?></a>
-                        <br>
-                        <a href="cart.php?remove=<?=$product['product_id']?>" class="remove">Remove</a>
-                    </td>
-                    <td class="price">Ksh <?=number_format($product['price'])?></td>
-                    <td class="quantity">
-                        <input type="number" name="quantity-<?=$product['product_id']?>" value="<?=$products_in_cart[$product['product_id']]?>" min="1" max="<?=$product['quantity']?>" placeholder="Quantity" required>
-                    </td>
-                    <td class="price">Ksh <?=number_format($product['price'] * $products_in_cart[$product['product_id']])?></td>
-                </tr>
-                <?php endforeach; ?>
-                <?php endif; ?>
-            </tbody>
-        </table>
-        <div class="subtotal">
-            <span class="text">Subtotal</span>
-            <span class="price">Ksh <?=number_format($subtotal)?></span>
-        </div>
-        <div class="buttons">
-            <input type="submit" value="Update" name="update">
-            <input type="submit" value="Place Order" name="placeorder">
-        </div>
+    <!-- Collapsible wrapper -->
+
+    <form action="display_search.php" method="get" class="d-flex input-group w-auto">
+      <input
+        type="search"
+        class="form-control rounded"
+        placeholder="Search products"
+        aria-label="Search"
+        aria-describedby="search-addon"
+        name="search_product"
+      />
+      <span class="input-group-text border-0" id="search-addon">
+      <input type="submit" name="search_data_product" value="search"/></i>
+      </span>
     </form>
-</div>
-</main>
+    
+    <!-- Right elements -->
+    <div class="d-flex align-items-center">
+      <!-- Icon -->
+      <a class="nav-link me-3" href="cart.php">
+        <i class="fas fa-shopping-cart"></i>
+        <span class="badge rounded-pill badge-notification bg-danger"><?=$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;?></span>
+      </a>
+
+      <?php
+            if (isset($_SESSION['username'])) {
+                echo "<div class='dropdown'>
+                <a class='dropdown-toggle d-flex align-items-center hidden-arrow' href='' id='profiledetails' role='button' data-mdb-toggle='dropdown' aria-expanded='false'>
+                <img class='rounded-circle' height='25' loading='lazy' src='imgs/profile.png'>
+                <b>".ucfirst(strtolower($_SESSION['username']))."</b>
+                </a>
+                <ul class='dropdown-menu dropdown-menu-end' aria-labelledby='profiledetails'>
+                    <li><a class='dropdown-item' href='profile.php'>My profile</a></li>
+                    <li><a class='dropdown-item' href='logout.php'>Logout</a></li>
+                </ul>
+                </div>";
+            }else {
+                echo "<button type='button' class='btn btn-link px-3 me-2'>
+                <a href='login.php'></i><b>Login </b>|<b> Signup</b></a>
+              </button>";
+            }   
+        ?>
+    </div>
+    <!-- Right elements -->
+    
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
+
+<section class="h-100 h-custom" style="background-color: #d2c9ff;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12">
+        <div class="card card-registration card-registration-2" style="border-radius: 15px;">
+          <div class="card-body p-0">
+            <div class="row g-0">
+              <div class="col-lg-8">
+                <div class="p-5">
+                  <div class="d-flex justify-content-between align-items-center mb-5">
+                    <h1 class="fw-bold mb-0 text-black">Shopping Cart</h1>
+                    <h6 class="mb-0 text-muted"><?=$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;?> items</h6>
+                  </div>
+                  <hr class="my-4">
+
+                  <?php if (empty($products)): ?>
+                    <h6 class="text-black mb-0 text-center">You have no products added in your Shopping Cart</h6>
+                  <?php else: ?>
+                  <?php foreach ($products as $product): ?>
+                  <div class="row mb-4 d-flex justify-content-between align-items-center">
+                    <div class="col-md-2 col-lg-2 col-xl-2">
+                      <img
+                        src="admin_page/actions/<?=$product['product_image']?>"
+                        class="img-fluid rounded-3" alt="<?=$product['product_name']?>">
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-3">
+                      <h6 class="text-black mb-0"><?=$product['product_name']?></h6>
+                    </div>
+                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                      <button class="btn btn-link px-2"
+                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                        <i class="fas fa-minus"></i>
+                      </button>
+
+                      <input id="form1" min="0" name="quantity" value="1" type="number"
+                        class="form-control form-control-sm" />
+
+                      <button class="btn btn-link px-2"
+                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                        <i class="fas fa-plus"></i>
+                      </button>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                      <h6 class="mb-0">Ksh <?=number_format($product['price'])?></h6>
+                    </div>
+                    <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                      <a href="cart.php?remove=<?=$product['product_id']?>" class="text-muted"><i class="fas fa-times"></i></a>
+                    </div>
+                  </div>
+
+                  <hr class="my-4">
+                  <?php endforeach; ?>
+                  <?php endif; ?>
+
+                  <div class="pt-5">
+                    <h6 class="mb-0"><a href="products.php" class="text-body"><i
+                          class="fas fa-long-arrow-alt-left me-2"></i>Back to shop</a></h6>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4 bg-grey">
+                <div class="p-5">
+                  <h3 class="fw-bold mb-5 mt-2 pt-1">Summary</h3>
+                  <hr class="my-4">
+
+                  <div class="d-flex justify-content-between mb-4">
+                    <h5 class="text-uppercase">items <?=$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;?></h5>
+                    <h5>Ksh <?=number_format($subtotal)?></h5>
+                  </div>
+                  <hr class="my-4">
+
+                  <h5 class="text-uppercase mb-3">Give code</h5>
+
+                  <div class="mb-5">
+                    <div class="form-outline">
+                      <input type="text" id="form3Examplea2" class="form-control form-control-lg" />
+                      <label class="form-label" for="form3Examplea2">Enter your code</label>
+                    </div>
+                  </div>
+
+                  <hr class="my-4">
+
+                  <div class="d-flex justify-content-between mb-5">
+                    <h5 class="text-uppercase">Total price</h5>
+                    <h5>Ksh <?=number_format($subtotal)?></h5>
+                  </div>
+
+                  <button type="button" class="btn btn-dark btn-block btn-lg"
+                    data-mdb-ripple-color="dark">Register</button>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 
 <?php view('page_footer') ?>
