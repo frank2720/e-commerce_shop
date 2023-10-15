16c813ae5fac421c57404b560850f42189583351
frank2720 <otienof534@gmail.com>
2023-10-15 21:54:00 +0300

    auth page updating

--- public/profile.php
+++ public/profile.php
@@ -5,62 +5,124 @@ require_login();
 
 ?>
 <?php view('page_header', ['title' => 'Profile']) ?>
-<header>
-    <div class="content-wrapper">
-        <h1>Pudfra-Shop</h1>
-        <nav>
-            <a href="home.php">Home</a>
-            <a href="products.php">Products</a>
-        </nav>
-        <div class="link-icons">
-            <a href="cart.php">
-                <i class="fas fa-shopping-cart"></i><span><?=$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;?></span>
-            </a>
-        </div>
-        
-        <div class="link-icons">
-            <?php
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+  <!-- Container wrapper -->
+  <div class="container">    
+    <!-- Toggle button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent1"
+      aria-controls="navbarSupportedContent1"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent1">      
+      <!-- Navbar brand -->
+      
+      <!-- Left links -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link " href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="products.php">Products</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-mdb-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Categories
+          </a>
+          <!-- Dropdown menu -->
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <?=getcategories()?>
+          </ul>
+        </li>
+      </ul>
+      <!-- Left links -->      
+    </div>
+    <!-- Collapsible wrapper -->
+    
+    <!-- Right elements -->
+    <div class="d-flex align-items-center">
+      <!-- Icon -->
+      <a class="nav-link me-3" href="cart.php">
+        <i class="fas fa-shopping-cart"></i>
+        <span class="badge rounded-pill badge-notification bg-danger"><?=$num_items_in_cart = isset($_SESSION['cart']) ? count($_SESSION['cart']) : 0;?></span>
+      </a>
+
+      <?php
             if (isset($_SESSION['username'])) {
                 echo "<div class='dropdown'>
-                <a class='nav-link dropdown-toggle' href='' id='profiledetails' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
-                <img style='height: 1.5em;display: inline-block' src='imgs/profile.png'>
-                <b>".ucfirst($_SESSION['username'])."</b>
+                <a class='dropdown-toggle d-flex align-items-center hidden-arrow' href='' id='profiledetails' role='button' data-mdb-toggle='dropdown' aria-expanded='false'>
+                <img class='rounded-circle' height='25' loading='lazy' src='imgs/profile.png'>
+                <b>".ucfirst(strtolower($_SESSION['username']))."</b>
                 </a>
-                <ul class='dropdown-menu' aria-labelledby='profiledetails'>
-                    <li><a class='dropdown-item' href='profile.php'>Account</a></li>
-                    <hr>
+                <ul class='dropdown-menu dropdown-menu-end' aria-labelledby='profiledetails'>
+                    <li><a class='dropdown-item' href='profile.php'>My profile</a></li>
                     <li><a class='dropdown-item' href='logout.php'>Logout</a></li>
                 </ul>
                 </div>";
             }else {
-                echo "<a href='login.php'></i><b>Login </b>|<b> Signup</b></a>";
+                echo "<button type='button' class='btn btn-link px-3 me-2'>
+                <a href='login.php'></i><b>Login </b>|<b> Signup</b></a>
+              </button>";
             }   
-            ?>
+        ?>
+    </div>
+    <!-- Right elements -->
+    
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
+
+<section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-12 col-xl-4">
+
+        <div class="card" style="border-radius: 15px;">
+          <div class="card-body text-center">
+            <div class="mt-3 mb-4">
+              <img src="imgs/profile.png"
+                class="rounded-circle img-fluid" style="width: 100px;" />
+            </div>
+            <h4 class="mb-2"><?= current_user() ?></h4>
+            <button type="button" class="btn btn-dark btn-rounded btn-lg">
+              Message now
+            </button>
+            <div class="d-flex justify-content-between text-center mt-5 mb-2">
+              <div>
+                <p class="mb-2 h5">8471</p>
+                <p class="text-muted mb-0">Wallets Balance</p>
+              </div>
+              <div class="px-3">
+                <p class="mb-2 h5">8512</p>
+                <p class="text-muted mb-0">Income amounts</p>
+              </div>
+              <div>
+                <p class="mb-2 h5">4751</p>
+                <p class="text-muted mb-0">Total Transactions</p>
+              </div>
+            </div>
+          </div>
         </div>
 
+      </div>
     </div>
-</header>
-<main>
-<div class="content">
-    <h2>Profile Page</h2>
-    <div class="loggedin">
-        <p>Your account details:</p>
-        <table>
-            <tr>
-                <td>Username:</td>
-                <td><?= current_user() ?></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td>.....</td>
-            </tr>
-            <tr>
-                <td>Phone number:</td>
-                <td>.......</td>
-            </tr>
-        </table>
-	</div>
-</div>
-</main>
-
+  </div>
+</section>
 <?php view('page_footer') ?>
