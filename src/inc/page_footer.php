f032e841f2333b3ebcdefb201795365de78917bf
frank2720 <otienof534@gmail.com>
2023-10-20 10:30:55 +0300

    footer design

--- src/inc/page_footer.php
+++ src/inc/page_footer.php
@@ -24,7 +24,7 @@
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold">Products</h6>
             <hr
@@ -47,7 +47,7 @@
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold">Useful links</h6>
             <hr
@@ -70,7 +70,7 @@
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3 mb-md-0 mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold">Contact</h6>
             <hr
