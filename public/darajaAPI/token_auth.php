8f3a499cb6648b0f8cc8d80f1dc1020bc2394821
frank2720 <otienof534@gmail.com>
2023-10-29 16:43:03 +0300

    darajaAPI intergration

---
+++ public/darajaAPI/token_auth.php
@@ -0,0 +1,18 @@
+<?php
+function token(){
+    $consumer_key = 'SjZbcsIER1TDuqchIaELunXYCKpdB76K';
+    $consumer_secret = 'EFnHQ1maJbY9fr5W';
+
+    $ch = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json;charset=utf-8'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_USERPWD, $consumer_key .':'. $consumer_secret);
+
+    $response = curl_exec($ch);
+    $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $response = json_decode($response);
+    curl_close($ch);
+    echo $response->access_token;
+}
+?>
